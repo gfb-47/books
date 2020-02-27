a288e01456dd811b715eb0f61353323a7ec4f1c3
Gabriel <gabrieelfbrito@gmail.com>
2020-02-27 10:04:43 -0300

    First commit

--- src/br/unitins/books/controller/LoginController.java
+++ src/br/unitins/books/controller/LoginController.java
@@ -9,7 +9,24 @@ import javax.inject.Named;
 @RequestScoped
 public class LoginController implements Serializable {
 
-
 	private static final long serialVersionUID = 3628538970641594570L;
+	private String login;
+	private String senha;
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
 }
---
+++ src/br/unitins/books/model/Usuario.java
@@ -0,0 +1,42 @@
+package br.unitins.books.model;
+
+public class Usuario {
+
+	private String nome;
+	private String senha;
+	private String email;
+	private String login;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+}
