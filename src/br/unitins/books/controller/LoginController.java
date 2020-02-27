cada59e7641cf030c7b0521fa5a7c085dc06dd24
Gabriel <gabrieelfbrito@gmail.com>
2020-02-27 09:00:32 -0300

    First commit

---
+++ src/br/unitins/books/controller/LoginController.java
@@ -0,0 +1,15 @@
+package br.unitins.books.controller;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class LoginController implements Serializable {
+
+
+	private static final long serialVersionUID = 3628538970641594570L;
+
+}
